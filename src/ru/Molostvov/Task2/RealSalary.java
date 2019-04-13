e421f5b1e7ee3ac0198fbcc040b771e8c72d9e54
Igor Molostvov <i.molostvov.stc@innopolis.university>
2019-04-13 13:19:59 +0300

    Task2

---
+++ src/ru/Molostvov/Task2/RealSalary.java
@@ -0,0 +1,20 @@
+package ru.Molostvov.Task2;
+
+import java.util.Scanner;
+
+public class RealSalary {
+
+    public static void main(String[] args) {
+        int salary;
+        int tax;
+        int realSalary;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите вашу зарплату: ");
+        salary = scanner.nextInt();
+        tax = salary*13/100;
+        realSalary = salary-tax;
+
+        System.out.println("После вычета налога вы получите: " + realSalary);
+    }
+}
