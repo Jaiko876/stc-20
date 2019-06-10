932d59234f1823ead1fb03911856467491e70abe
Igor Molostvov <i.molostvov.stc@innopolis.university>
2019-06-10 11:59:56 +0300

    task21

---
+++ src/ru/molostvov/task21/ShiftingArrElems.java
@@ -0,0 +1,33 @@
+package ru.molostvov.task21;
+
+
+import java.util.Arrays;
+
+public class ShiftingArrElems {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
+        int temp = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        // Сдвиг эллементов и замещение последнего эллемента 0м
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (j + 1 == arr[i].length) {
+                    arr[i][j] = 0;
+                } else {
+                    arr[i][j] = arr[i][j + 1];
+                }
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
