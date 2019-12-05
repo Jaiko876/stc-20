c941bdbf9dd8be060197af3878750285cec02e89
Jaiko876 <Jaiko876@gmail.com>
2019-12-05 18:11:22 +0300

    lesson11

---
+++ src/ru/stc20/molostvov/lesson11/GeneratorBuilder.java
@@ -0,0 +1,5 @@
+package ru.stc20.molostvov.lesson11;
+
+public interface GeneratorBuilder {
+    Generator create(int num);
+}
---
+++ src/ru/stc20/molostvov/lesson11/Maths.java
@@ -0,0 +1,7 @@
+package ru.stc20.molostvov.lesson11;
+
+import java.util.List;
+
+public interface Maths<T> {
+    void doMath(List<T> list);
+}
